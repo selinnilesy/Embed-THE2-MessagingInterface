9669865f0627f42f002633fb27c39c77c02bdaa7
Selin Yıldırım <38113970+selinnilesy@users.noreply.github.com>
2021-05-09 16:38:28 +0300

    v2

--- the2.asm
+++ the2.asm
@@ -84,6 +84,15 @@ disp_char0 res 1    ; on DISP4
 pressed_key res 1
 last_pressed_key res 1
 key_hit_counter res 1 ; keeps track of how many hits one key got in 1 second
+transition_flag res 1 
+thirty res 1
+thirty_counter res 1
+five res 1
+five_counter res 1
+rd0_state res 1	    ; state variable, bit0==0 => Button pressed, bit0==1 => Button not pressed,  bit1==1 => Button released
+rd1_state res 1
+rd2_state res 1
+rd3_state res 1
 
  
  
@@ -139,9 +148,8 @@ TABLE:
     RETLW b'01110101' ; k
     RETLW b'00111000' ; l
     RETLW b'01010101' ; m
+    RETLW b'01010100' ; n
     RETLW b'01011100' ; o
-TABLE2:
-    ADDWF PCL
     RETLW b'01110011' ; p  special calculation to fetch this and below ones
     RETLW b'01010000' ; r
     RETLW b'01100100' ; s
@@ -158,6 +166,7 @@ init:
     clrf zero
     clrf key_hit_counter
     clrf curr_index
+    clrf transition_flag
     setf nop_counter ; let nops iterate over 256 times
     movlw NOT_INITIALIZED_CHAR
     movwf not_initalized_char ; '_' variable
@@ -168,6 +177,9 @@ init:
     movff one, rb3_state ; bit0 is=1 not pressed, bit1=0 not released
     movlw 0x02
     movwf two
+    movlw 0x05
+    movwf five
+    movff five,five_counter
     movff twenty, twenty_secs
     movlw MESSAGE_WRITE
     movwf message_write
@@ -183,7 +195,10 @@ init:
     movff not_initalized_char, char5
     movff not_initalized_char, disp_char1
     movff not_initalized_char, disp_char0
-    movff not_initalized_char, last_pressed_key
+    setf last_pressed_key
+    movlw d'30'
+    movwf thirty
+    movff thirty, thirty_counter
     
     ;Disable interrupts
     clrf    INTCON
@@ -216,108 +231,6 @@ init:
     movwf   TMR0L
     goto initial_main
     
-poll_keypad:
-    clrf PORTB
-    clrf PORTD
-    movlw b'00011111' ;RB0,RB1,RB2 are output columns
-    movwf TRISB
-    movlw b'00001111' ;RD0,RD1,RD2,RD3 are input rows
-    movwf TRISD
-    ; poll RB0 column
-    btfsc PORTB, 0 
-    bra poll_r1_column ; needs to be low to determine
-     poll_1_4_7_star:
-	btfss PORTD, 0  ; if low, pressed.
-	retlw d'1' ; 1 is pressed
-	btfss PORTD, 1 
-	retlw d'4' ; 4 is pressed
-	btfss PORTD, 2
-	retlw d'7' ; 7 is pressed
-	btfss PORTD, 3 
-	retlw d'10' ; * is pressed
-    poll_r1_column:
-    btfsc PORTB, 1 
-    bra poll_r2_column
-     poll_2_5_8_0:
-	btfss PORTD, 0  ; if low, pressed.
-	retlw d'2' ; 2 is pressed
-	btfss PORTD, 1 
-	retlw d'5' ; 5 is pressed
-	btfss PORTD, 2
-	retlw d'8' ; 8 is pressed
-	btfss PORTD, 3 
-	retlw d'0' ; 0 is pressed
-    poll_r2_column:
-    btfsc PORTB, 2 
-    retlw NOT_INITIALIZED_CHAR ;   return not initalized if any value is not pressed. 
-    poll_3_6_9_square:
-	btfss PORTD, 0  ; if low, pressed.
-	retlw d'3' ; 3 is pressed
-	btfss PORTD, 1 
-	retlw d'6' ; 6 is pressed
-	btfss PORTD, 2
-	retlw d'9' ; 9 is pressed
-	btfss PORTD, 3 
-	retlw d'12' ; # is pressed
-
-which_char_to_display:  ; sets char0 to be displayed in porta. this subroutine is checked 78 times in a second.
-    movlw NOT_INITIALIZED_CHAR
-    cpfseq pressed_key		; if no pressed key, '_' will be displayed.
-    bra smthg_pressed		; something is pressed
-	 movf not_initalized_char, WREG
-	 cpfseq last_pressed_key    ; if there is no previosly pressed key within 1 sec, return '_' 
-	 bra smthg_pressed	   ; else keep displaying the same char as previous char. char0 needs to be returned again.
-	 movff not_initalized_char, char0
-	 return
-    smthg_pressed:    
-    movf last_pressed_key,WREG
-    cpfseq pressed_key	    ; if same key is pressed again within 1 sec, increment the hit counter.
-    bra another_key
-    bra same_key
-    same_key:
-	INCF key_hit_counter ; hit++ as the key is same as the previous one.
-    another_key:
-	movf not_initalized_char, WREG
-	cpfseq last_pressed_key    ; if there is no key press (idle) but we are in a key state, counter needs to be maintained.
-	bra new_key_press
-	bra calculate_wreg	  ; counter will be maintained so no changes exists for this one
-	new_key_press:
-	clrf key_hit_counter ; pressed keys might change within 1 sec, so this is a must ! 
-	movff pressed_key, last_pressed_key ; update the old pressed key now. 
-    
-    calculate_wreg:
-    ; now calculate the wreg for lookup table pc change.
-    movlw 0x03 ; i did not use sublw due to picsimlab bug
-    subwf key_hit_counter
-    btfsc STATUS, N ; if negative hit, add 3 back.
-    addwf key_hit_counter  ; keep the original value of the counter for later use in the same state !!! 
-    ; calculate wreg for look-up table
-    movlw d'7'
-    cpfslt pressed_key
-    bra special_treatment	; for the 16h and upper letters PRODH would be set.
-    decf pressed_key		;f or example, 2nd key is pressed, then wreg of lookup table will be 0,1,2 only for 3 chars.
-    decf pressed_key		; range of letters for a key formula ==> (key-2)*3
-    movlw 0x03
-    MULWF pressed_key		; multiplication results is in PRODL.
-    movf key_hit_counter, WREG	; move hit count to WREG.
-    addwf PRODL, WREG		;keep the result in wreg to index look-up table.
-    call TABLE			; fetches binary version of the corresponding char from the look-up table.
-    movwf char0
-    return
-    
-    special_treatment:
-    movlw 0x07
-    subwf pressed_key		; range of letters for a key formula ==> (key-7)*3 this time, to use only 1 byte again.
-    movlw 0x03
-    MULWF pressed_key		; multiplication results is again in PRODL.
-    movf key_hit_counter, WREG	
-    addwf PRODL, WREG		
-    call TABLE2			; check look-up table2 for p and above chars.
-    movwf char0
-    return
-    
-	
-    
 set_rb3_state:
     BCF rb3_state,1 ; reset released if exists !!!  
     btfss rb3_state , 0 ; what is rb3's state
@@ -336,6 +249,215 @@ set_rb3_state:
 	   setf rb3_state ; rb3_state[1]==1 means released
            BCF rb3_state,0  ; rb3_state[0]==1 means button not pressed anymore, other bits are not used anyways
 	return 
+set_rd0_state:
+    BCF rd0_state,1
+    btfss rd0_state , 0 
+    bra rd0_release_check  
+    bra rd0_press_check 
+    rd0_release_check: ; state==1 (not pressed)
+	btfsc PORTD,0
+	return
+	clrf rd0_state,0
+	return
+    rd0_press_check: ;state==0 (pressed)
+	btfsc PORTD,0
+	bra release_rd0
+	return
+	release_rd0:
+	   setf rd0_state 
+           BCF rd0_state,0  
+	return 
+set_rd1_state:
+    BCF rd1_state,1
+    btfss rd1_state , 0 
+    bra rd1_release_check  
+    bra rd1_press_check 
+    rd1_release_check: ; state==1 (not pressed)
+	btfsc PORTD,1
+	return
+	clrf rd1_state,0
+	return
+    rd1_press_check: ;state==0 (pressed)
+	btfsc PORTD,1
+	bra release_rd1
+	return
+	release_rd1:
+	   setf rd1_state 
+           BCF rd1_state,0  
+	return 
+set_rd2_state:
+    BCF rd2_state,1
+    btfss rd2_state , 0 
+    bra rd2_release_check  
+    bra rd2_press_check 
+    rd2_release_check: ; state==1 (not pressed)
+	btfsc PORTD,2
+	return
+	clrf rd2_state,0
+	return
+    rd2_press_check: ;state==0 (pressed)
+	btfsc PORTD,2
+	bra release_rd2
+	return
+	release_rd2:
+	   setf rd2_state 
+           BCF rd2_state,0  
+	return 
+set_rd3_state:
+    BCF rd3_state,1
+    btfss rd3_state , 0 
+    bra rd3_release_check  
+    bra rd3_press_check 
+    rd3_release_check: ; state==1 (not pressed)
+	btfsc PORTD,3
+	return
+	clrf rd3_state,0
+	return
+    rd3_press_check: ;state==0 (pressed)
+	btfsc PORTD,3
+	bra release_rd3
+	return
+	release_rd3:
+	   setf rd3_state 
+           BCF rd3_state,0  
+	return 	
+poll_keypad:
+    bcf PORTB, 0
+    bcf PORTB, 1 ; do not change input pins only clear output pins.
+    bcf PORTB, 2
+    clrf PORTD
+    
+    ; poll RB0 column
+    bcf PORTB, 0  ; set RB0=0 low
+    bsf PORTB, 1  
+    bsf PORTB, 2
+    movlw b'00001111' ;RD0,RD1,RD2,RD3 are input rows
+    movwf TRISD
+    nop nop nop nop nop nop
+     poll_1_4_7_star:
+	BTFSS PORTD, 3
+	retlw d'1' ; 1 is pressed
+	BTFSS PORTD, 2
+	retlw d'4' ; 4 is pressed
+	BTFSS PORTD, 1
+	retlw d'7' ; 7 is pressed
+	BTFSS PORTD, 0
+	retlw d'10' ; * is pressed
+	
+    clrf TRISD
+    clrf PORTD	; safe to change output port
+   
+    
+    
+    bcf PORTB, 1  ; set RB1=1 (LOW)
+    bsf PORTB, 0  
+    bsf PORTB, 2  
+    movlw b'00001111' ;RD0,RD1,RD2,RD3 are input rows
+    movwf TRISD
+    
+    nop nop nop nop nop nop
+     poll_2_5_8_0:
+	BTFSS PORTD, 3
+	retlw d'2'
+	BTFSS PORTD, 2
+	retlw d'5' 
+	BTFSS PORTD, 1
+	retlw d'8' 
+	BTFSS PORTD, 0
+	retlw d'0' 
+  
+    clrf TRISD
+    clrf PORTD
+    
+   
+    
+    bcf PORTB, 2  ; set RB2=1 (LOW)
+    bsf PORTB, 1  
+    bsf PORTB, 0  
+    movlw b'00001111' ;RD0,RD1,RD2,RD3 are input rows
+    movwf TRISD
+    nop nop nop nop nop nop
+    poll_3_6_9_square:
+	BTFSS PORTD, 3
+	retlw d'3'
+	BTFSS PORTD, 2
+	retlw d'6' 
+	BTFSS PORTD, 1
+	retlw d'9' 
+	BTFSS PORTD, 0
+	retlw d'12' 
+
+    retlw 0xFF ;   return 0xFF if any value is not pressed.
+
+which_char_to_display:  ; sets char0 to be displayed in porta. this subroutine is checked many times in a second.
+    movlw 0xFF
+    cpfseq pressed_key		; if no pressed key, '_' will be displayed.
+    bra smthg_pressed		; something is pressed
+	 cpfseq last_pressed_key    ; if there is no previosly pressed key within 1 sec, return '_' 
+	 bra smthg_pressed	   ; else keep displaying the same char as previous state's char. char0 needs to be returned again.
+	 movff not_initalized_char, char0
+	 return
+    smthg_pressed:      ; when curr keypad is 0xFF but prev 25ms loop's state is set keep printing the same char, or curr keypad is pressed
+    cpfseq last_pressed_key	    ; here record the very first key press to keep track of it in idle states.
+    bra not_the_very_first		    ; if very first key press, jump to another key config
+    very_first:
+    movff pressed_key,  last_pressed_key ; else, set the prev key as curr key to make sue of it later.
+    bra calculate_wreg
+    not_the_very_first:
+    movlw 0xFF
+    cpfseq pressed_key	    
+    bra new_key	    ; second key press in 1 second or  pressed on totally diff key in 1 sec
+    bra idle_now
+    idle_now:
+	movff last_pressed_key, pressed_key    ; if there is no key press (idle) but we are in a 's state, counter and curr displayed char needs to be maintained.
+	bra calculate_wreg   ; act like we,rre still in the previous state
+    new_key:
+	movf last_pressed_key, WREG ; to check if it is the same press with current's
+	cpfseq pressed_key
+	bra diff_key_press
+	same_key:		; if same key is pressed again within 1 sec, increment the hit counter.
+	INCF key_hit_counter ; hit++ as the key is same as the previous one.
+	movlw 0x03 ; i did not use sublw due to picsimlab bug
+	subwf key_hit_counter
+	btfsc STATUS, N ; if negative hit, add 3 back.
+	addwf key_hit_counter  ; else keep the original value of the counter for later use in the same state !!! 
+	bra calculate_wreg
+	diff_key_press:
+	    CLRF        key_hit_counter 
+	    movff	pressed_key, last_pressed_key
+	    movff	char4, char5	    ; swap chars in the display 
+	    movff	char3, char4
+	    movff	char2, char3
+	    movff	char1, char2
+	    movff	char0, char1	    ; char0 will be set in calculate_wreg
+	    movlw	NOT_INITIALIZED_CHAR  ; if char5 is still an uninitialized char, continue without transitioning ! 
+	    cpfseq	char5
+	    setf	transition_flag	    ; with this flag after setting char0 i will transition states before 1 sec expires.
+    
+    calculate_wreg:
+    ; calculate wreg for look-up table
+    movlw 0x10
+    cpfslt pressed_key		; only key 2 to key 9 has input letters on keypad.
+    return
+    decf pressed_key		; for example, 2nd key is pressed, then wreg of lookup table will be 0,1,2 only for 3 chars.
+    decf pressed_key		; range of letters for a key formula ==> (key-2)*3
+    movlw 0x03
+    MULWF pressed_key		; multiplication results is in PRODL.
+    INCF pressed_key
+    INCF pressed_key
+    movf key_hit_counter, WREG	; move hit count to WREG.
+    addwf PRODL, WREG		;keep the result in wreg to index look-up table.
+    RLNCF WREG,WREG		; multiply by 2 as instruction byte size
+    call TABLE			; fetches binary version of the corresponding char from the look-up table.
+    movwf char0
+    TSTFSZ  transition_flag	; if this new char0 fulls the total char count limit that is 6,
+    bra end_writing_state
+    return
+    end_writing_state:
+    movff   message_review , state  ; main will start with next state after displaying ends.
+    clrf transition_flag
+    return
+  
 initial_main:
     call set_rb3_state
     btfss rb3_state,1 ; repeat until released button is set
@@ -358,11 +480,11 @@ main:
 	    call poll_keypad ; check key press
 	    movwf pressed_key ; keep pressed key in pressed_key
 	    
-	    clrf PORTB
-	    movlw   b'00011000' ; RB3 and RB4 is input pin
-	    movwf   TRISB       ; TRISB = w_reg = b'00010000' 
-	    clrf   PORTD  ; clear PORTD
+	    bcf PORTB,0 ;RB0 cleaned 
+	    bcf PORTB,1 ;RB1 cleaned
+	    bcf PORTB,2 ;RB2 cleaned
 	    clrf   TRISD  ; PORTD is output
+	    clrf   PORTD  ; clear PORTD	; safe to change output port
 	    call which_char_to_display ; for all cases in 1 second this sets set char0.
 	    
 	movlw	0x14	    ; to check against 20
@@ -385,19 +507,18 @@ main:
 	    call TABLE_NUMS	
 	    bsf PORTA, 3	;  select display3
 	    movwf PORTD		
+	    
 	    waitx_x:
 	    INCF nop_counter	; wait 256 times to display  the digit enough amount of time 
 	    btfss STATUS, 3	; did overflow occur?
 	    goto waitx_x
 	    bcf PORTA, 3
-	    
 	    TSTFSZ twenty_secs ; if 0, since interrupt hits here, immediately continue next state.
 	    goto not_zero_yet
 	    goto time_expired
 	    not_zero_yet:
 		goto finish_loop  ; this also enables the intterup back again
 	    time_expired:
-		bsf PORTA, 3
 		movff   message_review , state  ; global counter, 20 secs, is expired. We will not visit mes_write anymore.
 		goto main  ; no need to enable the 20-seconds-interrupt again as we transition to next state
 	larger_than_ten:
@@ -408,16 +529,18 @@ main:
 	    call TABLE_NUMS	
 	    bsf PORTA,3
 	    movwf PORTD   ; second digit was kept in WREG
+	   
 	    wait1x_x:
 	    INCF nop_counter
 	    btfss STATUS, 3 ; did overflow occur?
-	    goto wait1x_x	   
+	    goto wait1x_x
 	    bcf PORTA, 3
 	    movlw 0x0A
 	    addwf twenty_secs ; put back temp subtracted amount
 	    movlw b'00000110'
 	    bsf PORTA, 2	    
 	    movwf PORTD   ; display "1" 
+	   
 	    wait1x_1:
 	    INCF nop_counter
 	    btfss STATUS, 3 ; did overflow occur?
@@ -428,24 +551,29 @@ main:
 	    movlw b'00000110' 
 	    bsf PORTA, 2
 	    movwf PORTD   ; display "1" 
+	   
 	    wait10_1:
 	    INCF nop_counter
 	    btfss STATUS, 3 ; did overflow occur?
 	    goto wait10_1
+	    
 	    bcf PORTA,2
 	    movlw b'00111111' 
 	    bsf PORTA,3
 	    movwf  PORTD    ; display "0" 
+	   
 	    wait10_0:
 	    INCF nop_counter
 	    btfss STATUS, 3 ; did overflow occur?
 	    goto wait10_0
+	    
 	    bcf PORTA,3
 	    goto    finish_loop
 	display_twenty:
 	    movlw b'01011011' 
 	    bsf PORTA, 2
 	    movwf PORTD   ; display "2" 
+	   
 	    wait20_2:
 	    INCF nop_counter
 	    btfss STATUS, 3 ; did overflow occur?
@@ -454,6 +582,7 @@ main:
 	    movlw b'00111111' 
 	    bsf PORTA,3
 	    movwf PORTD    ; display "0" 
+	   
 	    wait20_0:
 	    INCF nop_counter
 	    btfss STATUS, 3 ; did overflow occur?
@@ -461,19 +590,20 @@ main:
 	    bcf PORTA,3
 	finish_loop:
 	    bsf PORTA, 4	;  select display4
-	    movff not_initalized_char, PORTD	
-	    waitx_char1:
+	    movff char1, PORTD
+	    wait_char1:
 	    INCF nop_counter	
 	    btfss STATUS, 3	; did overflow occur?
-	    goto waitx_char1
+	    goto wait_char1
 	    bcf PORTA, 4
 	    
 	    bsf PORTA, 5	;  select display5
 	    movff char0, PORTD	
-	    waitx_char0:
+	    wait_char0:
 	    INCF nop_counter	
 	    btfss STATUS, 3	; did overflow occur?
-	    goto waitx_char0
+	    goto wait_char0
+	     
 	    bcf PORTA, 5
 	    goto    main
     mes_review:
@@ -493,6 +623,21 @@ timer_interrupt:
     goto	timer_interrupt_erroneous_exit    ;No, then exit from interrupt service routine
     decf	twenty_secs             ;Yes, then one second has passed
     clrf	key_hit_counter
+    setf	last_pressed_key	    ; clear this when switching seconds
+    movlw	NOT_INITIALIZED_CHAR  ; if char not entered do not save anything
+    cpfseq	char0		    ; if char entered needs to be saved after 1 sec, save it and shift the chars on display.
+    bra save_char
+    bra timer_interrupt_exit
+    save_char:	
+    movff	char4, char5	    ; swap chars in the display 
+    movff	char3, char4
+    movff	char2, char3
+    movff	char1, char2
+    movff	char0, char1
+    movlw	NOT_INITIALIZED_CHAR  ; if char5 is still an uninitialized char, continue without transitioning ! 
+    cpfseq	char5	    
+    movff       message_review , state  ; else, transition to next state ! 
+    
 timer_interrupt_exit:
     movlw   0x67      
     movwf   TMR0H
